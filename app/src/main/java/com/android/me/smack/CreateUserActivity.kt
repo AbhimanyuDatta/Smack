d9f23081a94e4605d73a6c4a6e373ab2f3b5e464
Abhimanyu Datta <a.datta47@gmail.com>
2019-03-21 23:43:11 +0530

    avatar and background color

--- app/src/main/java/com/android/me/smack/CreateUserActivity.kt
+++ app/src/main/java/com/android/me/smack/CreateUserActivity.kt
@@ -1,10 +1,20 @@
 package com.android.me.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.android.me.smack.util.COLOR_BOUND
+import com.android.me.smack.util.DRAWABLE
+import com.android.me.smack.util.IMAGE_BOUND
+import com.android.me.smack.util.THEME_BOUND
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +22,29 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
-
+        val random = Random()
+        val color = random.nextInt(THEME_BOUND)
+        val avatar = random.nextInt(IMAGE_BOUND)
+        userAvatar = if (color == 0) {
+            "light$avatar"
+        } else {
+            "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, DRAWABLE, packageName)
+        createAvatarImgView.setImageResource(resourceId)
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random()
+        val red = random.nextInt(COLOR_BOUND)
+        val green = random.nextInt(COLOR_BOUND)
+        val blue = random.nextInt(COLOR_BOUND)
+        createAvatarImgView.setBackgroundColor(Color.rgb(red, green, blue))
 
+        val savedRed = red.toDouble()/COLOR_BOUND
+        val savedGreen = green.toDouble()/COLOR_BOUND
+        val savedBlue = blue.toDouble()/COLOR_BOUND
+        avatarColor = "[$savedRed, $savedGreen, $savedBlue, 1]"
     }
 
     fun createUserClicked(view: View) {
---
+++ app/src/main/java/com/android/me/smack/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.android.me.smack.util
+
+const val DRAWABLE = "drawable"
+const val COLOR_BOUND = 255
+const val IMAGE_BOUND = 28
+const val THEME_BOUND = 2
