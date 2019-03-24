fbab0e9b682d693c84d77c22364f84e17edbe8b0
Abhimanyu Datta <a.datta47@gmail.com>
2019-03-24 11:32:55 +0530

    added channel dialog

--- app/src/main/java/com/android/me/smack/controller/MainActivity.kt
+++ app/src/main/java/com/android/me/smack/controller/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.android.me.smack.controller
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.android.me.smack.R
 import com.android.me.smack.service.AuthService
 import com.android.me.smack.service.UserDataService
 import com.android.me.smack.util.BROADCAST_USER_DATA_CHANGE
 import com.android.me.smack.util.DRAWABLE
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_channel_dialog.view.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 import kotlinx.android.synthetic.main.nav_header_main.view.*
@@ -35,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        hideKeyboard()
         // broadcast receiver
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
     }
@@ -76,11 +79,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addChannelClicked(view: View) {
+        if (AuthService.isLoggedIn) {
+            val builder = AlertDialog.Builder(this)
+            val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
+            builder.setView(dialogView)
+                .setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
+                    // perform logic when clicked
+                    val nameTextField = dialogView.addChannelNameTxt
+                    val descTextField = dialogView.addChannelDescTxt
+                    val channelName = nameTextField.text.toString()
+                    val channelDesc = descTextField.text.toString()
 
+                    // create channel with channel name and desc
+                    hideKeyboard()
+                }
+                .setNegativeButton("Cancel") { dialog: DialogInterface?, which: Int ->
+                    // cancel dialog
+                    hideKeyboard()
+                }
+                .show()
+        }
     }
 
     fun sendMsgBtnClicked(view: View) {
 
+    }
+
+    private fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 
 }
